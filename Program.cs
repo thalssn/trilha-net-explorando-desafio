896ce27802b25ed32faa0f213e0358477e49e023
Vinicius Lima <thalissonvini@outlook.com>
2025-08-22 13:15:09 -0300

    updatede: Menu interativo implementado

--- Program.cs
+++ Program.cs
@@ -3,23 +3,82 @@ using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
-List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+bool exibirMenu = true;
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Bem-vindo ao sistema de reservas do hotel!");
+    Console.WriteLine("Digite sua opção: \n");
+    Console.WriteLine("1 - Fazer reserva");
+    Console.WriteLine("2 - Consultar quartos disponíveis");
+    Console.WriteLine("3 - Encerrar \n");
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+    string entrada = Console.ReadLine();
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+    switch (entrada)
+    {
+        case "1":
+
+            List<Pessoa> hospedes = new List<Pessoa>();
+
+            Pessoa p1 = new Pessoa(nome: "Vinicius");
+            Pessoa p2 = new Pessoa(nome: "Thalisson");
+
+            hospedes.Add(p1);
+            hospedes.Add(p2);
+
+            Suite suite = new Suite(tipoSuite: "Premium", capacidade: 4, valorDiaria: 300);
+
+            Reserva reserva = new Reserva(diasReservados: 11);
+            reserva.CadastrarSuite(suite);
+            reserva.CadastrarHospedes(hospedes);
+
+            Console.WriteLine("\nDetalhes da Reserva\n");
+            Console.WriteLine($"Tipo da Suíte: {reserva.Suite.TipoSuite}");
+            Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+
+            foreach (Pessoa p in hospedes)
+            {
+                Console.WriteLine($"- {p.Nome}");
+            }
+
+            decimal valorTotal = reserva.CalcularValorDiaria();
+            decimal valorOriginal = reserva.DiasReservados * reserva.Suite.ValorDiaria;
+
+            Console.WriteLine($"Valor total da reserva: R${valorOriginal:N2}");
+
+            if (reserva.DiasReservados >= 10)
+            {
+                Console.WriteLine($"Desconto de 10% aplicado: R${valorOriginal - valorTotal:N2}");
+                Console.WriteLine($"Valor final com desconto: R${valorTotal:N2}");
+            }
+
+            Console.WriteLine("\nPressione uma tecla para continuar...");
+            Console.ReadLine();
+            break;
+
+        case "2":
+
+            Console.WriteLine("Opção 2 selecionada: Consultar Quartos Disponíveis.");
+            Suite suiteConsulta = new Suite(tipoSuite: "Premium", capacidade: 4, valorDiaria: 300);
+
+            Console.WriteLine($"Tipo da suite: {suiteConsulta.TipoSuite}, Capacidade: {suiteConsulta.Capacidade}, Preço: R${suiteConsulta.ValorDiaria}");
+            Console.WriteLine("\nPressione uma tecla para continuar...");
+            Console.ReadLine();
+            break;
+
+        case "3":
+            Console.WriteLine("Encerrando o programa...");
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida!");
+            Console.WriteLine("Pressione uma tecla para continuar...");
+            Console.ReadLine();
+            break;
+    }
+}
